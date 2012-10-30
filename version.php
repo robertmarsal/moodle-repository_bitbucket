1eb1276510c19a3333c39d05e8a40c8d166cf909
Robert Boloc <robertboloc@gmail.com>
2012-10-30 10:28:36 +0100

    changes version to match 2.1 stable branch

--- version.php
+++ version.php
@@ -27,6 +27,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2012102401;
-$plugin->requires  = 2011120505;
+$plugin->requires  = 2011070108;
 $plugin->component = 'repository_bitbucket';
 $plugin->release   = '1.0';
